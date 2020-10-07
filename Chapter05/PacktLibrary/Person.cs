d317daf6bff3367dcbb6cddc2a08f77c92240002
gpdn_admin <gpdn_admin@ViettelGroup.ViettelAD.com>
2020-10-07 17:01:56 +0700

    Storing data within fields

--- Chapter05/PacktLibrary/Person.cs
+++ Chapter05/PacktLibrary/Person.cs
@@ -1,13 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Packt.Shared
 {
-    public class Person: Object
+    public class Person : Object
     {
         public WondersOfTheAncientWorld FavoriteAncientWonder;
+        public WondersOfTheAncientWorld BucketList;
+        public List<Person> Children = new List<Person>();
         public string Name;
         public DateTime DateOfBirth;
-        public override string ToString() {
+        public const string Species = "Homo Sapien";
+        public readonly string HomePlanet = "Earth";
+        public readonly DateTime Instantiated;
+        public Person()
+        {
+            Name = "Unknown";
+            Instantiated = DateTime.Now;
+        }
+
+        public Person(string initialName, string homePlanet)
+        {
+            Name = initialName;
+            HomePlanet = homePlanet;
+            Instantiated = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
             return "OK";
         }
     }
--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -28,8 +28,52 @@ namespace PeopleApp
                 format: "{0}'s favorite wonder is {1}. It's integer is {2}.",
                 arg0: bob.Name,
                 arg1: bob.FavoriteAncientWonder,
-                arg2: (int) bob.FavoriteAncientWonder
+                arg2: (int)bob.FavoriteAncientWonder
             );
+
+            bob.BucketList = WondersOfTheAncientWorld.HangingGardensOfBabylon
+                | WondersOfTheAncientWorld.MausoleumAtHalicarnassus;
+            WriteLine($"{bob.Name}'s bucket list is {bob.BucketList}");
+            bob.Children.Add(new Person { Name = "Alfred" });
+            bob.Children.Add(new Person { Name = "Zoe" });
+            WriteLine($"{bob.Name} has {bob.Children.Count} children:");
+            for (int child = 0; child < bob.Children.Count; child++)
+            {
+                WriteLine($"{bob.Children[child].Name}");
+            }
+
+            BankAccount.InterestRate = 0.012M;
+            var jonesAccount = new BankAccount();
+            jonesAccount.AccountName = "Mrs. Jones";
+            jonesAccount.Balance = 2400;
+
+            WriteLine(format: "{0} earned {1:C} interest",
+                arg0: jonesAccount.AccountName,
+                arg1: jonesAccount.Balance * BankAccount.InterestRate);
+
+            var gerrierAccount = new BankAccount();
+            gerrierAccount.AccountName = "Ms. Gerrier";
+            gerrierAccount.Balance = 98;
+            WriteLine(format: "{0} earned {1:C} interest.",
+                arg0: gerrierAccount.AccountName,
+                arg1: gerrierAccount.Balance * BankAccount.InterestRate);
+
+            WriteLine($"{bob.Name} is a {Person.Species}");
+            WriteLine($"{bob.Name} was born on {bob.HomePlanet}");
+
+            var blankPerson = new Person();
+            WriteLine(
+                format: "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
+                arg0: blankPerson.Name,
+                arg1: blankPerson.HomePlanet,
+                arg2: blankPerson.Instantiated);
+
+            var gunny = new Person("Gunny", "Mars");
+            WriteLine(format:
+                "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
+                arg0: gunny.Name,
+                arg1: gunny.HomePlanet,
+                arg2: gunny.Instantiated);
         }
     }
 }
