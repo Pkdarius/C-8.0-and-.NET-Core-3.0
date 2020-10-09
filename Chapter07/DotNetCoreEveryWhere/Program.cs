799427850de972e14b111a4e26105e872294df93
Trinh Duy Hai <36527583+Pkdarius@users.noreply.github.com>
2020-10-09 17:24:13 +0700

    Publishing a self-contained app

---
+++ Chapter07/DotNetCoreEveryWhere/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+namespace DotNetCoreEveryWhere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("I can run everywhere!");
+        }
+    }
+}
